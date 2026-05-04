0b2c088146bf1707f0e584e4d7ed98a25d99f446
яна <imaksimmar@gmail.com>
2026-05-04 20:33:49 +0300

    class Iterator, operator[] instead of get/set

--- triangle_matrix.hpp
+++ triangle_matrix.hpp
@@ -9,21 +9,19 @@
 
 template <typename T, template <typename> class Container> 
 class TriangleMatrix {
-    private:
-    size_t size;
-    Container<T>* data;
-
     public:
 
-    class RowProxy
-    {
-        private:
-        TriangleMatrix<T, Container>* matrix;
-        size_t row;
+    class Iterator {
+    private:
+        TriangleMatrix<T, Container>* matrix; 
+        size_t row;                          
+        size_t rowStartIndex;                
 
-        public:
-        RowProxy(TriangleMatrix<T, Container>* m, size_t r)
-        : matrix(m), row(r) {}
+    public:
+        Iterator(TriangleMatrix<T, Container>* m, size_t r)
+        : matrix(m), row(r) {
+            rowStartIndex = row * (row + 1) / 2; 
+        }
 
         T& operator[](size_t column) {
             if(row>=matrix->GetSize() || column>=matrix->GetSize()) {
@@ -32,16 +30,60 @@ class TriangleMatrix {
             if(row<column) {
                 throw InvalidArgument(); 
             }
-
-            size_t index = row*(row+1)/2 + column;
-            return (*matrix->data)[index];
+            return (*matrix->data)[rowStartIndex + column];
         }
     };
 
-    RowProxy operator[](size_t row) {
-        return RowProxy(this, row);
+    
+    class ConstIterator {
+    private:
+        const TriangleMatrix<T, Container>* matrix;
+        size_t row;
+        size_t rowStartIndex;
+
+    public:
+        ConstIterator(const TriangleMatrix<T, Container>* m, size_t r)
+        : matrix(m), row(r) {
+            rowStartIndex = row * (row + 1) / 2;
+        }
+
+    
+        T operator[](size_t column) const {
+            if(row>=matrix->GetSize() || column>=matrix->GetSize()) {
+                throw IndexOutOfRange();
+            }
+            if(row<column) {
+                return T(); 
+            }
+            return (*matrix->data)[rowStartIndex + column];
+        }
+    };
+
+    Iterator operator[](size_t row) { return Iterator(this, row); }
+    ConstIterator operator[](size_t row) const { return ConstIterator(this, row); }
+
+    private:
+    size_t size;
+    Container<T>* data;
+
+    public:
+    TriangleMatrix() 
+    : size(0), data(new Container<T>()) {}
+
+    TriangleMatrix(size_t n) 
+    : size(n), data(new Container<T>())
+    {
+        for(size_t i=0; i<size; ++i) {
+            for(size_t j=0; j<=i; ++j) {
+                data->Append(T());
+            }
+        }
     }
 
+    TriangleMatrix(const TriangleMatrix& other)
+    : size(other.size), data(new Container<T>(*other.data)) {}
+
+    ~TriangleMatrix() { delete data; }
 
 
 };
